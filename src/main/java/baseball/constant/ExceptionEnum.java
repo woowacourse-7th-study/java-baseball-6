fc678d27d23d99f0d827f5b66361bcd89ec2a908
Kyuwon Jeong <32204027@dankook.ac.kr>
2024-08-23 12:12:14 +0900

    feat(ValidatorService, ExceptionEnum): 예외 케이스 추가

--- src/main/java/baseball/constant/ExceptionEnum.java
+++ src/main/java/baseball/constant/ExceptionEnum.java
@@ -2,7 +2,10 @@ package baseball.constant;
 
 public enum ExceptionEnum {
 
-    THREE("3자리 정수를 입력해주세요.");
+    THREE("3자리 정수를 입력해주세요."),
+    DUP("중복되지 않은 정수를 입력해주세요."),
+    RANGE("1부터 9까지의 정수를 입력해주세요."),
+    INT("정수를 입력해주세요.");
 
     private final String message;
 
--- src/main/java/baseball/service/ValidatorService.java
+++ src/main/java/baseball/service/ValidatorService.java
@@ -2,6 +2,7 @@ package baseball.service;
 
 import baseball.constant.ExceptionEnum;
 import baseball.constant.IndexEnum;
+import baseball.constant.RandomRangeEnum;
 
 public class ValidatorService {
 
@@ -13,6 +14,23 @@ public class ValidatorService {
         if (userInput.length() != IndexEnum.MAX.getValue()) {
             throw new IllegalArgumentException(ExceptionEnum.THREE.getMessage());
         }
+
+        if (userInput.charAt(0) == userInput.charAt(1) || userInput.charAt(1) == userInput.charAt(2) || userInput.charAt(0) == userInput.charAt(2)) {
+            throw new IllegalArgumentException(ExceptionEnum.DUP.getMessage());
+        }
+
+        for (int i=0; i<IndexEnum.MAX.getValue(); i++) {
+            if ((userInput.charAt(i) - '0') < RandomRangeEnum.START.getValue() && RandomRangeEnum.END.getValue() < userInput.charAt(i) - '0') {
+                throw new IllegalArgumentException(ExceptionEnum.RANGE.getMessage());
+            }
+        }
+
+        try {
+            Integer.parseInt(userInput);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(ExceptionEnum.INT.getMessage());
+        }
+
     }
 
 }
