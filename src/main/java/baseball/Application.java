400a74663b47723f40cf98eaefea9a0e6bdf9e38
JHZLO <010703kjh@gamil.com>
2024-08-24 00:37:35 +0900

    [feat](Application): 게임 시작 문구 출력

---
+++ src/main/java/baseball/Application.java
@@ -0,0 +1,10 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("숫자 야구 게임을 시작합니다."); // 게임 시작 메시지 출력
+    }
+}
